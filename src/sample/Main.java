aee59c39a4ba6471507f18e6be1f7b12fb0f2ed2
YuChenLiao <yuchenliao3@gmail.com>
2021-04-08 11:50:53 +0800

    重新绘制棋盘，解决边界判断

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -20,19 +20,18 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
-
-    private Integer chessPaneWidth = 780;
-    private Integer chessPaneHeight = 780;
-    private Integer chessPaneUnitWidth = 60;
-    private Integer chessPaneUnitHeight = 60;
-    private Integer startPosition = 60;
+    // 绘制棋盘的数据
+    private Integer chessPaneWidth = 570;// 780
+    private Integer chessPaneHeight = 570;
+    private Integer chessPaneUnitWidth = 30;
+    private Integer chessPaneUnitHeight = 30;
+    private Integer startPosition = 30;
 
     private AnchorPane anchorPane;
     private Boolean  isBlack = true;
     private Boolean  isSuccess = false;
-
-    private Integer[][]  data = new Integer[13][13];
-
+    // 储存对局信息的数组
+    private Integer[][]  data = new Integer[19][19];//[13][13]
 
     private String chessPaneLineColor = "#000000";
     private Stage primaryStage;
@@ -41,7 +40,6 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage = primaryStage;
         initData();
-
 
         this.anchorPane = new AnchorPane();
         Canvas canvas = new Canvas(chessPaneWidth,chessPaneHeight);
@@ -58,11 +56,9 @@ public class Main extends Application {
             }
         });
 
-
-
         Button button = new Button("重新开始");
-        button.setLayoutY(15);
-        button.setLayoutX(60);
+        button.setLayoutY(590);
+        button.setLayoutX(265);
         anchorPane.getChildren().add(button);
         button.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -70,7 +66,7 @@ public class Main extends Application {
 
                 isBlack = true;
                 isSuccess = false;
-                data = new Integer[13][13];
+                data = new Integer[19][19];
                 initData();
                 anchorPane.getChildren().removeAll(anchorPane.getChildren());
                 anchorPane.getChildren().add(canvas);
@@ -79,26 +75,21 @@ public class Main extends Application {
             }
         });
 
-
         primaryStage.setScene(scene);
-        primaryStage.setTitle("javafx版本简易五子棋");
-        primaryStage.setWidth(840);
-        primaryStage.setHeight(860);
+        primaryStage.setTitle("javafx五子棋");
+        primaryStage.setWidth(630);// 840
+        primaryStage.setHeight(680);// 860
         primaryStage.setResizable(false);
         primaryStage.show();
 
 
     }
 
-
-
-
-
     //初始化存储数据
     public void initData(){
 
-        for ( int i=0; i<13; i++ ){
-            for( int j=0; j<13; j++ ){
+        for ( int i=0; i<19; i++ ){
+            for( int j=0; j<19; j++ ){
                 data[i][j] = 0;
             }
         }
@@ -108,14 +99,10 @@ public class Main extends Application {
     public void saveData( Integer i, Integer j, Boolean isBlack ){
 
         i= i-1;j=j-1;
-        if ( 0 < i && i < 13  && j>0 && j<13 ){
-
+        if ( 0 <= i && i < 19  && j>=0 && j<19 ){
             data[i][j] = isBlack ? 1: 2;
         }
     }
-
-
-
     //展示胜利的弹框
     public void showSuccess( Stage primaryStage, String text ){
 
@@ -136,8 +123,6 @@ public class Main extends Application {
         label.setTextFill(Paint.valueOf("#333"));
         hBox.getChildren().add(label);
 
-
-
         stage.setScene(scene);
         stage.setWidth(300);
         stage.setHeight(200);
@@ -149,15 +134,11 @@ public class Main extends Application {
         stage.show();
 
     }
-
-
     //检测鼠标点击位置
     public void calculateMouseClickPosition( double x, double y ){
-
         if ( isSuccess ){
             return;
         }
-
         //有效点击位置计算
         Integer availableMinX = chessPaneUnitWidth/2;
         Integer availableMinY = chessPaneUnitHeight/2;
@@ -168,10 +149,12 @@ public class Main extends Application {
         if ( availableMinX <= x && x <= availableMaxX && availableMinY <= y && y <= availabelMaxY ){
 
             //计算出其所在行，所在列
-            double translateX = x -30;
-            double translateY = y -30;
-            Integer i =  (int)Math.ceil(translateX/60+0.000001);
-            Integer j =  (int)Math.ceil(translateY/60+0.000001);
+            double translateX = x -15;
+            double translateY = y -15;
+            Integer i =  (int)Math.ceil(translateX/30+0.000001);
+            System.out.println("X:"+i);
+            Integer j =  (int)Math.ceil(translateY/30+0.000001);
+            System.out.println("Y:"+j);
 
 
             //存储位置
@@ -192,17 +175,13 @@ public class Main extends Application {
         }
 
     }
-
-
     //计算是否胜利
     public void calculateIsFinish( Integer xi, Integer yj, Boolean isBlack ){
-
         xi = xi-1; yj = yj-1;
         //如果是黑子，那么存储值是1；
         //如果是白子，那么存储值是2；
         Integer saveValue = isBlack? 1: 2;
         //根据最后一个点的落点位置，计算八个方位是否有呈5个一致的棋子。
-
 
         Integer[][] validateData = new Integer[4][9];
         for ( int i=0; i<4 ; i++ ){
@@ -211,19 +190,16 @@ public class Main extends Application {
             }
         }
 
-
         //获取竖向
         int k=0;
         for( int i=yj-4; i<yj+5; i++ ){
 
-            if ( i>=0 && i<13 ){
+            if ( i>=0 && i<19 ){
 
                 validateData[0][k] = data[xi][i];
             }
             k++;
         }
-
-
 
         Integer leftCount = 0;
         Boolean isLeftCount = true;
@@ -237,15 +213,11 @@ public class Main extends Application {
             }else {
                 isLeftCount = false;
             }
-
-            System.out.println( validateData[0][9-i-1] + " "+saveValue );
-            System.out.println(leftCount+" "+ rightCount + " "+ isLeftCount + " "+ isRightCount);
             if( validateData[0][9-i-1] == saveValue && isRightCount ){
                 rightCount ++;
             }else{
                 isRightCount = false;
             }
-
 
             if ( leftCount + rightCount >= 6 ){
 
@@ -254,9 +226,6 @@ public class Main extends Application {
             }
         }
 
-
-
-
         //获取横向
         k=0;
         leftCount = 0;
@@ -264,13 +233,12 @@ public class Main extends Application {
         isLeftCount = true;
         isRightCount = true;
         for ( int i=xi-4; i<xi+5; i++  ){
-            if ( i>=0 && i < 13 ){
+            if ( i>=0 && i < 19 ){
 
                 validateData[1][k] = data[i][yj];
             }
             k++;
         }
-
 
         for ( int i = 4; i>=0; i-- ){
 
@@ -294,8 +262,6 @@ public class Main extends Application {
             }
         }
 
-
-
         //获取左斜线
         k=0;
         leftCount = 0;
@@ -305,14 +271,13 @@ public class Main extends Application {
         for ( int i=xi-4,j=yj-4; i<xi+5 && j<yj+5; i++,j++  ) {
 
             //i表示列的index， j表示行的index
-            if ( i>=0 && i<13 && j>=0 && j<13 ){
+            if ( i>=0 && i<19 && j>=0 && j<19 ){
                 validateData[2][k] = data[i][j];
             }
             k++;
 
         }
         for ( int i = 4; i>=0; i-- ){
-
             //分别从左，右两边试探
             if ( validateData[2][i] == saveValue && isLeftCount ){
                 leftCount ++;
@@ -326,14 +291,12 @@ public class Main extends Application {
                 isRightCount = false;
             }
 
-
             if ( leftCount + rightCount >= 6 ){
 
                 isSuccess = true;
                 showSuccess(primaryStage, (isBlack?"黑色":"白色") + "获得胜利");
             }
         }
-
 
         //获取右斜线
         k=0;
@@ -344,14 +307,12 @@ public class Main extends Application {
         for ( int i=xi-4,j=yj+4; i<xi+5 && j>yj-5; i++,j--  ) {
 
             //i表示列的index， j表示行的index
-            if ( i>=0 && i<13 && j>=0 && j<13 ){
+            if ( i>=0 && i<19 && j>=0 && j<19 ){
                 validateData[3][k] = data[i][j];
             }
             k++;
 
         }
-
-
 
         for ( int i = 4; i>=0; i-- ){
 
@@ -361,7 +322,6 @@ public class Main extends Application {
             }else {
                 isLeftCount = false;
             }
-
 
             if( validateData[3][9-i-1] == saveValue && isRightCount ){
                 rightCount ++;
@@ -375,20 +335,8 @@ public class Main extends Application {
                 showSuccess(primaryStage, (isBlack?"黑色":"白色") + "获得胜利");
             }
         }
-
-
-        for( int i=0; i<4; i++ ){
-            for( int j=0; j<9; j++ ){
-                System.out.print(validateData[i][j]);
-            }
-            System.out.println();
-        }
-
-        System.out.println();
     }
-
-
-
+    // 绘制白棋
     public void drawWhiteChessUnit( AnchorPane anchorPane, Integer i, Integer j ){
 
         Button button =  new Button();
@@ -396,8 +344,7 @@ public class Main extends Application {
         button.setStyle("-fx-background-radius: 30; -fx-effect:dropshadow(three-pass-box, #72b9da, 8.0,0, 0, 0); ");
         drawChessUnit(button, i, j);
     }
-
-
+    // 绘制黑棋
     public void drawBlackChessUnit( AnchorPane anchorPane, Integer i, Integer j ){
 
         Button button =  new Button();
@@ -405,7 +352,7 @@ public class Main extends Application {
         button.setStyle("-fx-background-color: #000;-fx-background-radius: 30; -fx-effect:dropshadow(three-pass-box, #72b9da, 8.0,0, 0, 0);  ");
         drawChessUnit(button, i, j);
     }
-
+    // 绘制棋子位置
     public void drawChessUnit( Button button, Integer i, Integer j ) {
 
         Integer width = chessPaneUnitWidth;
@@ -414,28 +361,21 @@ public class Main extends Application {
         button.setLayoutX(i*chessPaneUnitWidth-width/2);
         button.setLayoutY(j*chessPaneUnitWidth-width/2);
     }
-
-
-
+    // 绘制棋盘
     public void drawChessPane( Canvas canvas ){
 
         GraphicsContext gc = canvas.getGraphicsContext2D();
 
-        for ( int i=0; i<13; i++ ){
+        for ( int i=0; i<19; i++ ){
             gc.setStroke(Paint.valueOf(chessPaneLineColor));
             gc.setLineWidth(1);
             gc.strokeLine(startPosition, (i+1)*chessPaneUnitHeight, chessPaneWidth,(i+1)*chessPaneUnitHeight);
             gc.strokeLine((i+1)*chessPaneUnitWidth, startPosition, (i+1)*chessPaneUnitWidth,chessPaneHeight);
         }
-
-
-
-
     }
 
-
     public static void main(String[] args) {
-//主方法设置
+    // 主方法设置
         launch(Main.class,args);
     }
 }
